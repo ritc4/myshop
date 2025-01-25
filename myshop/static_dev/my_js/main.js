51263f0310492995b15336c1d2791aa3238b0d09
Куксин Александр <ritc4@rambler.ru>
2025-01-25 22:19:30 +0300

    creat static_dev and templates

---
+++ myshop/static_dev/my_js/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+  // Обработка события прокрутки
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $("#top").fadeIn(); // Показываем кнопку "вверх"
+    } else {
+      $("#top").fadeOut(); // Скрываем кнопку "вверх"
+    }
+  });
+
+  // Обработка клика по кнопке "вверх"
+  $("#top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 500); // Прокручиваем к верху страницы
+    return false; // Предотвращаем стандартное поведение
+  });
+
+  // Инициализация карусели Owl Carousel
+  $(".own-carousel-full").owlCarousel({
+    margin: 20,
+    responsive: {
+      0: {
+        items: 1, // Для экранов шириной 0px и выше
+      },
+      768: {
+        items: 2, // Для экранов шириной 768px и выше
+      },
+      992: {
+        items: 3, // Для экранов шириной 992px и выше
+      },
+    },
+  });
+});
+
+
+const offcanvasCartEl = document.getElementById("offcanvasCart");
+const offcanvasCart = new bootstrap.Offcanvas(offcanvasCartEl);
+document.getElementById('cart-open').addEventListener('click',(e) =>{
+    e.preventDefault();
+    offcanvasCart.toggle();
+
+});
+
+document.querySelectorAll('.closecart').forEach(item =>{
+    item.addEventListener('click',(e) =>{
+        e.preventDefault();
+        offcanvasCart.hide();
+        let href = item.href.split("#").pop();
+        document.getElementById(href).scrollIntoView();
+    });
+});
+
+
+
+
+// const offcanvasCartEl = document.getElementById("offcanvasCart");
+// const offcanvasCart = new bootstrap.Offcanvas(offcanvasCartEl);
+
+// document.querySelectorAll(".closecart").forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     e.preventDefault;
+//     offcanvasCart.hide();
+//     let href = item.href.split("#").pop();
+//     offcanvasCartEl.addEventListener("hidden.bs.offcanvas", () => {
+//       document.getElementById(href).scrollIntoView();
+//     });
+//   });
+// });
